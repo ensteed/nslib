c2561547f0dae661f639599d46f0fb4409850aac
dprandle <daniel@noblesteed.dev>
2026-05-21 06:10:22 -0500

    Use orthographic camera and fix depth projection

--- src/math/matrix4.h
+++ src/math/matrix4.h
@@ -211,13 +211,12 @@ matrix4<T> ortho(T left, T right, T top, T bottom, T nearz, T farz)
 
     T x = (right + left) / w;
     T y = (top + bottom) / h;
-    T z = (farz + nearz) / p;
 
     matrix4<T> ret;
-    set_mat_column(&ret, 3, vector4<T>{-x, -y, -z, 1});
+    set_mat_column(&ret, 3, vector4<T>{-x, -y, -nearz / p, 1});
     ret[0][0] = 2 / w;
     ret[1][1] = 2 / h;
-    ret[2][2] = 2 / p;
+    ret[2][2] = 1 / p;
     return ret;
 }
 
@@ -229,8 +228,8 @@ matrix4<T> perspective_vfov(T fov, T aspect_ratio, T z_near, T z_far)
     matrix4<T> ret{};
     ret[0][0] = height * (T(1) / aspect_ratio);
     ret[1][1] = height;
-    ret[2][2] = (z_far + z_near) / z_range;
-    ret[2][3] = T(-2) * z_far * z_near / z_range;
+    ret[2][2] = z_far / z_range;
+    ret[2][3] = -z_far * z_near / z_range;
     ret[3][2] = T(1);
     ret[3][3] = T(0);
     return ret;
@@ -244,8 +243,8 @@ matrix4<T> perspective_hfov(T fov_h_deg, T aspect_ratio, T z_near, T z_far)
     matrix4<T> ret{};
     ret[0][0] = fx;
     ret[1][1] = fx * aspect_ratio; // fy = fx * (w/h)
-    ret[2][2] = (z_far + z_near) / z_range;
-    ret[2][3] = (T(-2) * z_far * z_near) / z_range;
+    ret[2][2] = z_far / z_range;
+    ret[2][3] = -z_far * z_near / z_range;
     ret[3][2] = T(1);
     ret[3][3] = T(0);
     return ret;
